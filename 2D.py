bab0945105818e5f00c174d9caea47a68c0611fb
Shasheen8 <50681308+Shasheen8@users.noreply.github.com>
2019-11-15 17:36:15 -0500

    Add files via upload

---
+++ 2D.py
@@ -0,0 +1,16 @@
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+matrix[1][2]= 35
+print(matrix[2][1])
+
+matrix = [
+    [7,4,6],
+    [1,4,7],
+    [3,5,2]
+]
+for row in matrix:
+    for item in row:
+        print(item)
---
+++ arithmetic.py
@@ -0,0 +1,22 @@
+print(10 + 3)
+print(10 - 3)
+print(10/3)
+print(10//3)
+print(10*3)
+print(10**3)
+print(10%3)
+
+
+
+x= 10
+x= x + 3
+print(x)
+
+y =10
+y -= 3
+print(y)
+
+#arithmetic assignment operator
+
+s= (7+5)* 2**3 +4
+print(s)
